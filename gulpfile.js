ed4d2dfb1c1ada1caa3fc0c69319fa9bbe568b4b
Ondrej Prenek <ondrej.prenek@zentity.com>
2018-02-26 00:14:43 +0100

    init page + gulp installed

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create(),
+    gitignore = require('gulp-gitignore');
+
+var DEST = 'build/';
+
+gulp.task('scripts', function() {
+    return gulp.src('src/js/*.js')
+      .pipe(concat('custom.js'))
+      .pipe(gulp.dest(DEST+'/js'))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(uglify())
+      .pipe(gulp.dest(DEST+'/js'))
+      .pipe(browserSync.stream());
+});
+
+var compileSASS = function (filename, options) {
+  return sass('src/scss/*.scss', options)
+        .pipe(autoprefixer('last 2 versions', '> 5%'))
+        .pipe(concat(filename))
+        .pipe(gulp.dest(DEST+'/css'))
+        .pipe(browserSync.stream());
+};
+
+gulp.task('sass', function() {
+    return compileSASS('custom.css', {});
+});
+
+gulp.task('sass-minify', function() {
+    return compileSASS('custom.min.css', {style: 'compressed'});
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        },
+        startPath: './index.html'
+    });
+});
+
+gulp.task('gitignore', function () {
+    return gulp.src('src/**/*')
+        // exclude files defined in .gitignore
+        .pipe(gitignore())
+        .pipe(gulp.dest(''));
+});
+
+gulp.task('watch', function() {
+  // Watch .html files
+  gulp.watch('./*.html', browserSync.reload);
+  // Watch .js files
+  gulp.watch('src/js/*.js', ['scripts']);
+  // Watch .scss files
+  gulp.watch('src/scss/*.scss', ['sass', 'sass-minify']);
+});
+
+// Default Task
+gulp.task('default', ['browser-sync', 'watch']);
